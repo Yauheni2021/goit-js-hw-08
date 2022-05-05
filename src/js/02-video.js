855444d858cc2146c1d859cb7db66f3e35f41fe2
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-05-05 19:55:30 +0200

    add video-2 js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onPlay, 1000));
+addEventListener('DOMContentLoaded', pageInit);
+
+function onPlay(data) {
+    const currentTime = JSON.stringify(data.seconds);
+    localStorage.setItem(STORAGE_KEY, currentTime);
+};
+
+function pageInit() {
+    const savedTime = localStorage.getItem(STORAGE_KEY);
+
+    if (savedTime) {
+        player.setCurrentTime(JSON.parse(savedTime));
+    } 
+}
